1e067df5f59a5b26e685fb4c384cafc7a0465216
ishakboufatah <boufatah.ishak@gmail.com>
2021-07-06 14:36:17 +0100

    inputs

--- pset1/a.py
+++ pset1/a.py
@@ -1,1 +1,10 @@
 #python file
+"""
+Created on Tus JUIL  6 14:18:48 2021
+
+@author: ishak
+"""
+annual_salary= int(input('Enter your annual salary:'))
+portion_saved= float(input('Enter the percent of your salary to save, as a decimal:'))
+total_cost= int(input("Enter the cost of your dream home:"))
+:
