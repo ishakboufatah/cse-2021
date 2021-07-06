c13230abcfd3999e18ada912443b938835ac7260
ishakboufatah <boufatah.ishak@gmail.com>
2021-07-06 14:42:04 +0100

    variable

--- pset1/a.py
+++ pset1/a.py
@@ -7,4 +7,9 @@ Created on Tus JUIL  6 14:18:48 2021
 annual_salary= int(input('Enter your annual salary:'))
 portion_saved= float(input('Enter the percent of your salary to save, as a decimal:'))
 total_cost= int(input("Enter the cost of your dream home:"))
-:
+monthly_salary=annual_salary/12
+portion_down_payment=0.25
+current_savings=0
+r = 0.04
+Number_of_months=1
+
