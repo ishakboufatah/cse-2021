a1e3c2c8ee7b9250103441c2130c6be1708bfdd0
ishakboufatah <boufatah.ishak@gmail.com>
2021-07-07 12:00:09 +0100

    update while loop

--- pset1/b.py
+++ pset1/b.py
@@ -7,19 +7,21 @@ print('Hi :)')
 annual_salary= int(input('Enter your annual salary:'))
 portion_saved= float(input('Enter the percent of your salary to save, as a decimal:'))
 total_cost= int(input("Enter the cost of your dream home:"))
-semi_annual_raise= int(input("Enter the semiannual raise, as a decimal:"))
+semi_annual_raise= float(input("Enter the semiannual raise, as a decimal:"))
 
 
-monthly_salary=annual_salary/12
 portion_down_payment=0.25
 current_savings=0
 r = 0.04 
 Number_of_months=1
 while current_savings<total_cost*(portion_down_payment):
+    monthly_salary=annual_salary/12
     Number_of_months=Number_of_months+1
     current_savings=current_savings+monthly_salary*portion_saved
     current_savings=current_savings*r/12+current_savings
+    if Number_of_months%6 == 0:
+        annual_salary=annual_salary+annual_salary*semi_annual_raise
 
-print(current_savings)
-print(Number_of_months)
+print('Number of months:',Number_of_months)
+
 
