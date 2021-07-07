fa8f2952a357783559f56a20c7b71f7a3e5ea9dc
ishakboufatah <boufatah.ishak@gmail.com>
2021-07-07 11:45:46 +0100

    add semi_annual_raise input

--- pset1/b.py
+++ pset1/b.py
@@ -7,6 +7,8 @@ print('Hi :)')
 annual_salary= int(input('Enter your annual salary:'))
 portion_saved= float(input('Enter the percent of your salary to save, as a decimal:'))
 total_cost= int(input("Enter the cost of your dream home:"))
+semi_annual_raise= int(input("Enter the semiannual raise, as a decimal:"))
+
 
 monthly_salary=annual_salary/12
 portion_down_payment=0.25
@@ -15,7 +17,7 @@ r = 0.04
 Number_of_months=1
 while current_savings<total_cost*(portion_down_payment):
     Number_of_months=Number_of_months+1
-    current_savings=current_savings+monthly_salary*portion_saved/100
+    current_savings=current_savings+monthly_salary*portion_saved
     current_savings=current_savings*r/12+current_savings
 
 print(current_savings)
